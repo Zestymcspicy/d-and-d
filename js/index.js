484d5f14c9de3e6adb3a505015e04a60e4d7ef0e
Harold Beckerman <halbeckerman@yahoo.com>
2019-04-08 23:46:31 -0500

    need to build add user form

--- js/index.js
+++ js/index.js
@@ -5,13 +5,13 @@ function addCharacter() {
   const name = document.getElementById('nameInput');
   const level = document.getElementById('levelInput');
   const charClass = document.getElementById('classInput');
-    const newChar = {
-      name: name.value,
-      level: Number(level.value),
-      class: charClass.value
-    };
-    const myJSON = JSON.stringify(newChar)
-    console.log(myJSON);
+    // const newChar = {
+    //   name: name.value,
+    //   level: Number(level.value),
+    //   class: charClass.value
+    // };
+    // const myJSON = JSON.stringify(newChar)
+    // console.log(myJSON);
     fetch(`${url}/characters/create/`, {
       method: 'POST',
       body: `name=${name.value}&level=${level.value}&class=${charClass.value}`,
